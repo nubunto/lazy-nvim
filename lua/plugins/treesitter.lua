975ca9e81cdd0a5fe083c8d607e0540609a96b10
Bruno Panuto <bl.panuto@gmail.com>
2023-04-06 11:40:33 -0300

    ignore "help" treesitter parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,8 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
+      opts.ignore_install = { "help" }
+
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
           "vimdoc",
