17135e878cf113a6d055773ba4f96f4cef7fc18c
Bruno Silva <bl.panuto@gmail.com>
2023-04-11 21:07:19 -0300

    use hop instead of leap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,52 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map({ "n" }, "<leader>o", "<cmd>Neotree toggle<cr>")
+map({ "n" }, "<leader>c", "<cmd>bd<cr>")
+
+-- Hop keymaps
+local hop = require("hop")
+local directions = require("hop.hint").HintDirection
+
+map({ "" }, "f", function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, { remap = true })
+
+map({ "" }, "F", function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, { remap = true })
+
+map({ "" }, "t", function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, { remap = true })
+
+map({ "" }, "T", function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, { remap = true })
+
+map({ "n" }, "s", function()
+  hop.hint_char2({ direction = directions.AFTER_CURSOR })
+end)
+
+map({ "n" }, "S", function()
+  hop.hint_char2({ direction = directions.BEFORE_CURSOR })
+end)
+
+map({ "n" }, "l", function()
+  hop.hint_lines_skip_whitespace({ direction = directions.AFTER_CURSOR })
+end)
+
+map({ "n" }, "L", function()
+  hop.hint_lines_skip_whitespace({ direction = directions.BEFORE_CURSOR })
+end)
---
+++ lua/plugins/config.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "lunarvim/Onedarker.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedarker",
+    },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+
+  {
+    "ggandor/flit.nvim",
+    enabled = false,
+  },
+  {
+    "ggandor/leap.nvim",
+    enabled = false,
+  },
+
+  {
+    "phaazon/hop.nvim",
+    branch = "v2",
+    config = function()
+      require("hop").setup({})
+    end,
+    event = "BufEnter",
+  },
+}
