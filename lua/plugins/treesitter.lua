ab0e3e6d9631adc61e1a2ea04660551f0c806df1
Bruno Panuto <bl.panuto@gmail.com>
2023-04-12 10:29:44 -0300

    update lazy-lock

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,9 @@ return {
 
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
+          "bash",
           "lua",
+          "yaml",
           "go",
           "java",
           "elixir",
