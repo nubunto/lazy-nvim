a000ce098b2179966b6f72db4caba285a813642e
Bruno Panuto <bl.panuto@gmail.com>
2023-04-19 15:36:45 -0300

    ui changes

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,21 +1,48 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = function(_, opts)
-    opts.default_component_configs.symbols = {
+  {
+    "rcarriga/nvim-notify",
+    enabled = false,
+  },
+
+  {
+
+    "folke/noice.nvim",
+    enabled = false,
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.default_component_configs.symbols = {
         -- Change type
-        added     = "✚",
-        deleted   = "✖",
-        modified  = "",
-        renamed   = "",
+        added = "✚",
+        deleted = "✖",
+        modified = "",
+        renamed = "",
 
         -- Status type
         untracked = "",
-        ignored   = "",
-        unstaged  = "",
-        staged    = "",
-        conflict  = "",
-    }
-    opts.close_if_last_window = true
-    opts.log_level = "error"
-  end,
+        ignored = "",
+        unstaged = "",
+        staged = "",
+        conflict = "",
+      }
+      opts.window = { position = "right" }
+      opts.close_if_last_window = true
+      opts.log_level = "error"
+    end,
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        offsets = {
+          {
+            text_align = "right",
+          },
+        },
+      },
+    },
+  },
 }
