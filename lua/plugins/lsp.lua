f3d105115efd6cbdc20babfbd6923266349971a2
Bruno Panuto <bl.panuto@gmail.com>
2023-04-19 15:28:36 -0300

    add denols, and reconfigure tsserver

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+local util = require('null-ls.utils')
 return {
   {
     "neovim/nvim-lspconfig",
@@ -7,6 +8,13 @@ return {
         -- jdtls requires java 17, but I can't use it :)
         -- jdtls = {},
         elixirls = {},
+        denols = {
+          root_dir = util.root_pattern("deno.json", "deno.jsonc")
+        },
+        tsserver = {
+          single_file_support = false,
+          root_dir = util.root_pattern("package.json")
+        }
       },
     },
   },
