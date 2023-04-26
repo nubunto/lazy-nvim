4e6a75ee8e2132c4bc2b11ed5ae2ee5c3a7b36b8
Bruno Panuto <bl.panuto@gmail.com>
2023-04-26 00:22:45 -0300

    remove unused harpoon ui

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -50,13 +50,11 @@ end)
 
 local wk = require('which-key')
 local mark = require('harpoon.mark')
-local ui = require('harpoon.ui')
-
 
 wk.register({
   ["<leader>j"] = {
     name = "+harpoon",
-    m = {
+    a = {
       function ()
         mark.add_file()
       end,
