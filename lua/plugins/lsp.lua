c21686574e20968cf90a7e5a30f0a94bb991c35e
Bruno Panuto <bl.panuto@gmail.com>
2023-09-04 13:21:07 -0300

    feat: format with prettier on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,34 @@
-local util = require('null-ls.utils')
+local util = require("null-ls.utils")
+local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return {
+        sources = {
+          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.formatting.prettier,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({
+              group = augroup,
+              buffer = bufnr,
+            })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
+          end
+        end,
+      }
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -9,13 +38,13 @@ return {
         -- jdtls = {},
         elixirls = {},
         denols = {
-          root_dir = util.root_pattern("deno.json", "deno.jsonc")
+          root_dir = util.root_pattern("deno.json", "deno.jsonc"),
         },
         tsserver = {
           single_file_support = false,
-          root_dir = util.root_pattern("package.json")
+          root_dir = util.root_pattern("package.json"),
         },
-        gopls = {}
+        gopls = {},
       },
     },
   },
